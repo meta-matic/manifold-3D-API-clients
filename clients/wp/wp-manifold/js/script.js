38069b3602a6fcf770efe6d2c822f6220528bae5
Pushkar Paranjpe <pushkar@locus.sh>
2018-02-22 16:26:22 +0530

    Updated the WP plugin UI

--- clients/wp/wp-manifold/js/script.js
+++ clients/wp/wp-manifold/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function (e) {
 
 	var base_url = "http://manifold.metamatic.us/v1/compute/";
-
+	var task_id = null;
 	var poller_id = null;
 	$("#loader_upload").hide();
 	$("#loader_compute").hide();
 
 	$("#uploadForm").on('submit',(function(e) {
 		e.preventDefault();
-		$("#taskID").html("");
+		$("#taskID").text("");
 		$("#result").html("");
 		console.log("Uploading ...");
 		$("#loader_upload").show();
@@ -20,19 +20,22 @@ $(document).ready(function (e) {
 		    cache: false,
 		    processData:false,
 		    success: function(data){
-			$("#loader_upload").hide();
-			$("#taskID").html(data.task_id);
-			$("#loader_compute").show();
-			console.log("Geting result ...");
-			console.log(data.task_id);
-			get_result();
+				$("#loader_upload").hide();
+				$("#taskID").attr("href",
+				 "http://manifold.metamatic.us/v1/compute/?task_id="+data.task_id);
+				task_id = data.task_id;
+				$("#taskID").text("Permalink");
+				$("#loader_compute").show();
+				console.log("Geting result ...");
+				console.log(data.task_id);
+				get_result();
 		    }
 		});
 	}));
 
 	function get_result() {
 		$.ajax({
-		    url: base_url + "?task_id=" + $("#taskID").html(),
+		    url: base_url + "?task_id=" + task_id,
 		    success: function(data){
 			if(data == ""){
 				if(poller_id == null){
@@ -44,6 +47,15 @@ $(document).ready(function (e) {
 				console.log(data);
 				var source   = $("#entry-template").html();
 				var template = Handlebars.compile(source);
+
+				// Round to 1 decimal place
+				data.volume.value = Math.round( data.volume.value * 10 ) / 10;
+				data.bbox.value.length = Math.round( data.bbox.value.length * 10 ) / 10;
+				data.bbox.value.width = Math.round( data.bbox.value.width * 10 ) / 10;
+				data.bbox.value.height = Math.round( data.bbox.value.height * 10 ) / 10;
+				data.time.value.max = Math.round( data.time.value.max * 10 ) / 10;
+				data.time.value.min = Math.round( data.time.value.min * 10 ) / 10;
+
 				var html = template(data);
 				$("#entry-rendered").html(html);
 				$("#loader_compute").hide();
--- clients/wp/wp-manifold/wp-manifold.php
+++ clients/wp/wp-manifold/wp-manifold.php
@@ -17,51 +17,58 @@ Text Domain: wp-manifold
 function ManifoldShortcode() {
 	$loading_img_src = plugins_url('css/images/loading@2x.gif', __FILE__ );
 
-	return '<table border="1px">
+	return  '<h4>Upload</h4>'.
+			'<img id="loader_upload" src="'. $loading_img_src .'" width="20px" height="20px">'.
+			'<form id="uploadForm">'.
+				'<input name="api_key" type="hidden" value="' . get_option('manifold_api_key') . '" />'.
+				'<table><tr>
+							<td><input name="datafile" type="file" /></td>
+							<td><input type="submit" value="Submit" /></td>
+				</tr></table>'.
+			'</form>'.
+
+			'<h4>Result</h4>'.
+			'<img id="loader_compute" src="'. $loading_img_src .'" width="20px" height="20px">'.
+			'<a href="#" id="taskID"></a>'.
+			'<table border="1px">
 			<tr>
 			<td>
-			<table width="100%"><tr><td width="50%"><h3>Upload</h3></td><td><img id="loader_upload" src="'. $loading_img_src .'" width="20px" height="20px"></td></tr></table>
-			</td>
-			<td>
-			<table width="100%"><tr><td width="50%"><h3>Compute</h3></td><td><img id="loader_compute" src="'. $loading_img_src .'" width="20px" height="20px"></td></tr></table>
-			</td>
-			<td>
-			<h3>Result</h3>
-			</td>
-			</tr>
-			<tr>
-			<td width="30%">
-				<form id="uploadForm">
-				<table>
-				<tr><td>
-				<input name="datafile" type="file" />
-				</td>
-				</tr>
-				<tr><td>
-				<input name="api_key" type="hidden" value="' . get_option('manifold_api_key') . '" />
-				</td><td>
-				<input type="submit" value="Submit" />
-				</td></tr>
-				</table>
-				</form>
-			</td>
-			<td>
-				<table width="100%">
-				<tr>
-				<td><label>Task ID:</label></td>
-				<td width="60%"><span id="taskID"></span></td>
-				</tr>
-				</table>
-			</td>
-			<td width="40%">
 				<span id="result">No result</span>
+				
 				<script id="entry-template" type="text/x-handlebars-template">
-				  <div class="entry"><em>Volume:</em> {{ volume.value }} {{ volume.UOM }}</div>
-				  <div class="entry"><em>Bounding box:</em> {{ bbox.value.length }}, {{ bbox.value.width }}, {{ bbox.value.height }}, {{ bbox.UOM }}</div>
-				  <div class="entry"><em>Facets:</em> {{ facet_count.value }}</div>
-				  <div class="entry"><em>Build time:</em> {{ time.value.min }} - {{ time.value.max }} {{ time.UOM }}</div>
-				  <div class="entry"><em>Image:</em> <img src="{{ root_url }}{{ image.value }}"></div>
+				  <table>
+				  <tr>
+				  	<th>Volume</th>
+				  	<th>Bounding box</th>
+				  	<th>Build time</th>
+				  </tr>
+				  <tr>
+				  <td>
+				  	<div class="entry">{{ volume.value }} {{ volume.UOM }}</div>
+				  </td>
+				  <td>
+				  	<div class="entry">{{ bbox.value.length }}, {{ bbox.value.width }}, {{ bbox.value.height }}, {{ bbox.UOM }}</div>
+				  </td>
+				  <td>
+					  <div class="entry">{{ time.value.min }} - {{ time.value.max }} {{ time.UOM }}</div>
+				  </td>
+				  </tr>
+				  </table>
+				  <table border="1px">
+				  	<tr>
+				  		<td>
+				  			<div class="entry"><em>Image XY:</em> <img src="{{ root_url }}{{ orthogonal_images.value.[0] }}"></div>
+						</td>
+				  		<td>
+				  			<div class="entry"><em>Image YZ:</em> <img src="{{ root_url }}{{ orthogonal_images.value.[1] }}"></div>
+						</td>
+						<td>
+				  			<div class="entry"><em>Image XZ:</em> <img src="{{ root_url }}{{ orthogonal_images.value.[2] }}"></div>
+						</td>
+				  	</tr>
+				  </table>
 				</script>
+
 				<div id="entry-rendered"></div>
 			</td>
 			</tr>
