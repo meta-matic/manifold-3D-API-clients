e0b9ff682f977922c742af3f400e18d79e8a89fc
Pushkar Paranjpe <pushkarparanjpe@gmail.com>
2018-03-13 18:35:42 +0530

    Made PHP client compat >= PHP5.5

--- clients/php/post.php
+++ clients/php/post.php
@@ -1,17 +1,18 @@
 <?php
-
 $target_url = 'http://manifold.metamatic.us/v1/compute/';
-
 $api_key = 'dummy-api-key';
 
-$file_name_with_full_path = realpath('../models/40mmcube.stl');
+$file_name_with_full_path = realpath('/tmp/40mmcube.stl');
+echo $file_name_with_full_path;
+echo "\n";
 
-$post = array('api_key' => $api_key,'datafile'=>'@'.$file_name_with_full_path);
+$post = array('api_key' => $api_key,'datafile'=>new CURLFile($file_name_with_full_path));
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$target_url);
 curl_setopt($ch, CURLOPT_POST,1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
 $result=curl_exec ($ch);
 curl_close ($ch);
 
